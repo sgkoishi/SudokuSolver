de4e8b988bcd8019ba6f69bed260f79fcdc34642
SGKoishi <sgkoishi@hotmail.com>
2019-02-28 21:14:29 -0800

    move control namespace

--- SudokuSolver/TransparentDataGridView.cs
+++ SudokuSolver/TransparentDataGridView.cs
@@ -6,29 +6,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Chireiden.SudokuSolver
+namespace System.Windows.Forms
 {
     /// <summary>
     /// https://stackoverflow.com/questions/1330220/set-datagrid-view-background-to-transparent
     /// </summary>
     public class TransparentDataGridView : DataGridView
     {
+        public Image Screenshot;
         protected override void PaintBackground(Graphics graphics, Rectangle clipBounds, Rectangle gridBounds)
         {
             base.PaintBackground(graphics, clipBounds, gridBounds);
-            var rectSource = new Rectangle(this.Location.X, this.Location.Y, this.Width, this.Height);
-            var rectDest = new Rectangle(0, 0, rectSource.Width, rectSource.Height);
-            var b = new Bitmap(this.Parent.ClientRectangle.Width, this.Parent.ClientRectangle.Height);
-            Graphics.FromImage(b).DrawImage(this.Parent.BackgroundImage, this.Parent.ClientRectangle);
-            graphics.DrawImage(b, rectDest, rectSource, GraphicsUnit.Pixel);
-
-            this.EnableHeadersVisualStyles = false;
-            this.ColumnHeadersDefaultCellStyle.BackColor = Color.Transparent;
-            this.RowHeadersDefaultCellStyle.BackColor = Color.Transparent;
-            foreach (DataGridViewColumn col in this.Columns)
+            if (this.Screenshot != null)
             {
-                col.DefaultCellStyle.BackColor = Color.Transparent;
-                col.DefaultCellStyle.SelectionBackColor = Color.Transparent;
+                var rectSource = new Rectangle(this.Location.X, this.Location.Y, this.Width, this.Height);
+                var rectDest = new Rectangle(0, 0, rectSource.Width, rectSource.Height);
+                var b = new Bitmap(this.Parent.ClientRectangle.Width, this.Parent.ClientRectangle.Height);
+                Graphics.FromImage(b).DrawImage(this.Screenshot, this.Parent.ClientRectangle);
+                graphics.DrawImage(b, rectDest, rectSource, GraphicsUnit.Pixel);
+
+                this.EnableHeadersVisualStyles = false;
+                this.ColumnHeadersDefaultCellStyle.BackColor = Color.Transparent;
+                this.RowHeadersDefaultCellStyle.BackColor = Color.Transparent;
+                foreach (DataGridViewColumn col in this.Columns)
+                {
+                    col.DefaultCellStyle.BackColor = Color.Transparent;
+                    col.DefaultCellStyle.SelectionBackColor = Color.Transparent;
+                }
             }
         }
     }
